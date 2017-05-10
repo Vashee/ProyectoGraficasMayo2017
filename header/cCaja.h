f286dbe2b19a7d00e3a1b19ac93af382621a1b2c
Itzi Reyes <itzireyes11@gmail.com>
2017-05-10 14:44:28 -0500

    Header caja x y z

--- header/cCaja.h
+++ header/cCaja.h
@@ -13,35 +13,39 @@
 #include <math.h>
 #endif
 
-#include "glm.h" 
+#include "glm.h"
 #include "vecs.h"
 #include "cForklift.h"
 
 class cCaja
 {
 public:
-	cCaja();
-	~cCaja();
-	void draw();
-	GLMmodel* caja;
-	float pos[3];
-	void checkFloorCollisions();
-	void stepSimulation(float dt);
-	void addForce(float *force);
-	bool inCollision(cForklift* other);
-
+    cCaja(float _x, float _y, float _z);
+    ~cCaja();
+    void draw();
+    GLMmodel* caja;
+    float x;
+    float y;
+    float z;
+    float pos[3];
+    void checkFloorCollisions();
+    void stepSimulation(float dt);
+    void addForce(float *force);
+    bool inCollision(cForklift* other);
+    bool visible;
+    
 private:
-	void clearForces();
-	void integrateVerlet(float dt);
-
-	float mass;
-
-	float oldPos[3];
-	float forces[3];
-	float origForces[3];
-	float dragForce[3];
-
-	float radius;
-	float restitutionCoefficient;
+    void clearForces();
+    void integrateVerlet(float dt);
+    
+    float mass;
+    
+    float oldPos[3];
+    float forces[3];
+    float origForces[3];
+    float dragForce[3];
+    
+    float radius;
+    float restitutionCoefficient;
 };
 
